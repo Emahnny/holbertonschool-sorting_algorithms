2539a409c43256bb25a21d3a536b873801860712
Michael A. Hernandez <michael.he30@hotmail.com>
2024-04-09 18:39:47 -0400

    Fix compilation problem

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,28 +13,31 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current, *temp, *temp_prev;
 
 	if (!list || !*list || !(*list)->next)
+	{
 		return;
-
+	}
 	for (current = (*list)->next; current != NULL; current = current->next)
+	{
 		temp = current;
 		temp_prev = temp->prev;
-
 		while (temp_prev && temp->n < temp_prev->n)
-
+		{
 			temp->prev->next = temp->next;
 			if (temp->next != NULL)
+			{
 				temp->next->prev = temp->prev;
-
+			}
 			temp->next = temp_prev;
 			temp->prev = temp_prev->prev;
-
 			if (temp->prev != NULL)
+			{
 				temp->prev->next = temp;
+			}
 			else
+			{
 				*list = temp;
-
+			}
 			temp->next->prev = temp;
-
 			temp_prev = temp->prev;
 			print_list(*list);
 		}
